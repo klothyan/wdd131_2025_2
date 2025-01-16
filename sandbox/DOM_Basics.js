4c3bc0a544ff414e8aba2d46a3248ed5df61ccfa
klothyan <icecreamy17@gmail.com>
2025-01-16 09:33:16 -0700

    newList complete

--- sandbox/DOM_Basics.js
+++ sandbox/DOM_Basics.js
@@ -1,17 +1,27 @@
 const newParagraph = document.createElement('p');
 newParagraph.innerText = 'Added with Javascript!';
-document.body.appendChild(newParagraph);
+
 
 const newImage = document.createElement('img');
 newImage.src = 'https://picsum.photos/200';
 newImage.setAttribute('alt', 'Random image from picsum.');
-document.body.appendChild(newImage);
+
 
 const newDiv = document.createElement('div');
 newDiv.innerHTML = '<ul><li>One</li><li>Two</li><li>Three</li></ul>';
-document.body.appendChild(newDiv);
+
 
 const ingredientsData = ["Pinto beans", "Korn", "Spices", "Tortillas"];
 const portionData = ["1 15oz can", "1 15oz can", "1Tbsp", "8"];
+
+
+function ingredientTemplate(index) {
+    return `<li>${portionData[index]}  ${ingredientsData[index]}</li>`
+}
+
+const newList = document.createElement("ul");
+ingredientsData.forEach(function (item, index) {
+    newList.innerHTML += ingredientTemplate(index);
+})
 
 
