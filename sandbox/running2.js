e1c588340469d61f5789ed920beca56d6266cc41
klothyan <icecreamy17@gmail.com>
2025-01-27 10:02:25 -0700

    Updated running2.js- (added LuckyNumber and such.)

--- sandbox/running2.js
+++ sandbox/running2.js
@@ -5,3 +5,17 @@ let total = myApples + friendApples;
 document.getElementById("myAppleElement").textContent = myApples;
 document.getElementById("friendAppleElement").textContent = friendApples;
 document.getElementById("totalApplesElement").textContent = total;
+
+const myArray = [12, 34, 21, 54];
+const luckyNumber = [21];
+let luckyIndex = -1;
+myArray.forEach(function (item, index) {
+    if (item === luckyNumber) {
+        luckyIndex = index;
+    }
+});
+
+function isLucky(item) {
+    return item === luckyNumber;
+}
+const index = myArray.findIndex(isLucky);
