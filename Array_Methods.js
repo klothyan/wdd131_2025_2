a818c33b212d895229104e2cd8c1d8f14a2bce34
klothyan <icecreamy17@gmail.com>
2025-02-01 16:34:56 -0700

    Fixed js

--- Array_Methods.js
+++ Array_Methods.js
@@ -1,32 +1,28 @@
 const steps = ["one", "two", "three"];
 const listTemplate = (step) => {
     return `<li>${step}</li>`;
-  };  
+};  
 const stepsHtml = steps.map(listTemplate);
 document.querySelector("#myList").innerHTML = stepsHtml.join("");
 
-const grade = ["A", "B", "C"];
-function convertGrade (grade) {
+const grades = ["A", "B", "C"];
+
+function convertGrade(grade) {
     let points = 0;
     if (grade === "A") {
         points = 4;
     } else if (grade === "B") {
         points = 3;
+    } else if (grade === "C") {
+        points = 2;
     }
     return points;
 }
-const gradesPoints = grade.map(convertGrade);
 
 const gpaPoints = grades.map(convertGrade);
-const points = gpaPoints.reduce(function (total, item) {
-  return total + item;
-});
+
+const pointsTotal = gpaPoints.reduce((total, item) => total + item, 0);
 const gpa = pointsTotal / gpaPoints.length;
 
-// example 2
-// this is the same thing as above, but with an arrow function
-const pointsTotal = gpaPoints.reduce((total, item) => total + item);
-const gpa = pointsTotal / gpaPoints.length;
+console.log("GPA:", gpa);
 
-// this could be further simplified as
-const gpa = gpaPoints.reduce((total, item) => total + item) / gpaPoints.length;
